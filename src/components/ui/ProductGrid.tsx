873393e8eb1c82f3757dfece4abb12f31481850a
Lucas Melendez <138076426+lucassmelendez@users.noreply.github.com>
2025-05-06 22:17:47 -0400

    mejora product grids

--- src/components/ui/ProductGrid.tsx
+++ src/components/ui/ProductGrid.tsx
@@ -20,7 +20,7 @@ export default function ProductGrid({ productos, title }: ProductGridProps) {
     <div>
       {title && <h2 className="text-2xl font-bold text-gray-800 dark:text-gray-200 mb-6">{title}</h2>}
       
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {productos.map((producto) => (
           <ProductCard key={producto.id_producto} producto={producto} />
         ))}
